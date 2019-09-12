d15c86052acefd25c951c610685df0580a7af62c
ryan.y.yang <ryan.y.yang@outlook.com>
2019-09-12 10:12:11 -0500

    asdfasdf

--- train/play-record.py
+++ train/play-record.py
@@ -6,6 +6,9 @@ from matplotlib import pyplot as plt
 from matplotlib import animation
 import re
 import os
+import queue
+import threading
+import readchar
 
 ##################################################
 # Specify the arena config here
@@ -16,6 +19,7 @@ arenaConfig = '../examples/configs/1-Food.yaml'
 # Create a folder to store data of this task
 ##################################################
 match = re.search('/([0-9A-Za-z\-]+?)\.yaml', arenaConfig)
+directoryName = None
 if match:
     directoryName = './' + match.group(1)
     if not os.path.exists(directoryName):
@@ -40,6 +44,9 @@ docker_target_name = None
 no_graphics = False
 n_arenas = 1
 resolution = 84
+
+
+
 
 ##################################################
 # Start up unity environment.
@@ -67,18 +74,73 @@ info = env.reset(arenas_configurations=arena_config_in)
 
 
 ##################################################
+# Start a new thread for IO
+##################################################
+queueFB = queue.Queue()
+queueLR = queue.Queue()
+
+
+def equalIgnoreCase(a, b):
+    return a.lower() == b.lower()
+
+
+def insertQ(q, x):
+    try:
+        q.put_nowait(x)
+    except Exception as e:
+        print(e)
+
+
+def inputReceiver():
+    while True:
+        char = readchar.readchar()
+        if equalIgnoreCase('A', char):
+            insertQ(queueLR, 1)
+        elif equalIgnoreCase('D', char):
+            insertQ(queueLR, 2)
+        elif equalIgnoreCase('W', char):
+            insertQ(queueFB, 1)
+        elif equalIgnoreCase('S', char):
+            insertQ(queueFB, 2)
+
+
+receiver = threading.Thread(target = inputReceiver)
+receiver.setDaemon(True)
+receiver.start()
+
+
+##################################################
 # Visualization
 ##################################################
 fig, ax = plt.subplots()
 ax.axis("off")
 image = ax.imshow(np.zeros((resolution, resolution, 3)))
 
+
 def initialize_animation():
     image.set_data(np.zeros((resolution, resolution, 3)))
 
+def getAction():
+    lr = 0
+    fb = 0
+    try:
+        lr = queueLR.get_nowait()
+    except Exception as e:
+        print(e)
+
+    try:
+        fb = queueFB.get_nowait()
+    except Exception as e:
+        print(e)
+
+    return np.array([fb, lr])
+
 
 def run_step_imshow(step):
-    res = env.step(np.random.randint(0, 3, size=2 * n_arenas))
+
+    action = getAction()
+
+    res = env.step(action)
     fig.suptitle('Step = ' + str(step))
     image.set_data(res['Learner'].visual_observations[0][0, :, :, :])
 
@@ -86,6 +148,7 @@ def run_step_imshow(step):
         env.reset()
 
     return image
+
 
 try:
     anim = animation.FuncAnimation(fig, run_step_imshow, init_func=initialize_animation, interval=50, repeat=False)
