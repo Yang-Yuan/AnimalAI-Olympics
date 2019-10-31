6bcdc34c50c2d639fd9419a04e84f7d337f38a9d
ryan.y.yang <ryan.y.yang@outlook.com>
2019-10-30 23:02:55 -0500

    asdfasdf

--- agents/chaser.py
+++ agents/chaser.py
@@ -72,24 +72,29 @@ class Chaser(object):
         path, _ = self.finder.find_path(start, end, grid)
 
         # if cannot find a path, then declare the failure of chasing and set an empty action
+        # this will lead to agent to start searching again.
         if path is None or len(path) <= 1:
             warnings.warn("Can't find a path to the target!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
             self.agent.current_action = AgentConstants.taxi
             self.agent.chase_failed = True
             return
 
+        # record the most recent path
         self.newest_path = path
+        # set action!
         self.agent.current_action = self.generate_action(path, target_idx, target_size)
 
-    # def is_new_critical_point_in_path(self, line_idx, idx0, idx1):
-    #     return 0 <= idx0 < AgentConstants.resolution and 0 <= idx1 < AgentConstants.resolution \
-    #            and (not self.agent.is_inaccessible[idx0, idx1]) \
-    #            and (not (np.array(line_idx) == [idx0, idx1]).all(axis=1).any())
-
     def generate_action(self, path, target_idx, target_size):
+        '''
+        This method generates the action for the agent given a path to the target,
+        the position of the target and the size of the target.
+        :param path:
+        :param target_idx:
+        :param target_size:
+        :return: action
+        '''
 
-        # min_col = 0
-        # max_col = 83
+        # find the first accessible pixel in the bottom row from left to right
         min_col = None
         for jj in np.arange(AgentConstants.resolution):
             if not self.agent.is_inaccessible[83, jj]:
@@ -101,6 +106,7 @@ class Chaser(object):
             self.agent.chase_failed = True
             return
 
+        # find the first accessible pixel in the bottom row from right to left
         max_col = None
         for jj in np.arange(AgentConstants.resolution)[::-1]:
             if not self.agent.is_inaccessible[83, jj]:
@@ -112,10 +118,20 @@ class Chaser(object):
             self.agent.chase_failed = True
             return
 
+        # start is where the agent is standing, and end will be furthest pixel on the path
+        # where the agent can reach by going straight  without pass any inaccessible pixel.
         start = path[0]
         end = path[1]
         for point in path[1:]:
             clear = True
+            # test if it will pass any inaccessible pixel going straight.
+            # To do  this, we draw lines (by bresenham algorithm), to see if
+            # the lines intersect inaccessible pixels.
+            # We do this incrementally from the second pixel (path[1]) to see how far we can go.
+            # This does not necessarily give the furthest pixel, but it can give us a feasible solution
+            # in a computationally acceptable way. Actually, if I try to compute the real furthest
+            # going-going straight reachable pixel, the online testing will be terminated due to the
+            # time limit for the first two categories.
             line_seg_idx = tuple(np.array(list(bresenham(83, min_col, point[1], point[0]))).transpose())
             line_seg_is_inaccessible = self.agent.is_inaccessible[line_seg_idx]
             if line_seg_is_inaccessible.any():
@@ -130,16 +146,19 @@ class Chaser(object):
             else:
                 break
 
+        # record the most recent furthest going-straight reachable pixel
         self.newest_end = end
 
+        # if it is not the target position, then we should not use the target_size
+        # to decide the action. So, set it to a defualt size.
         if (target_idx[::-1] != end).any():
             target_size = AgentConstants.size_limit
 
-        # self.newest_path = list(bresenham(AgentConstants.standpoint[1], AgentConstants.standpoint[0], end[0], end[1])) \
-        #                    + path[path.index(end) + 1:]
-
+        # the diff vector between the standpoint and the furthest going-straight reachable pixel
         direction_vec = np.array(end) - np.array(start)
 
+        # the following is simply a rule-based way to decide the action
+        # it is not good as a control system, but it works in most cases.
         if direction_vec[0] < -AgentConstants.aim_error_limit * (1 + np.exp(-target_size / AgentConstants.hl)):
             if target_size < AgentConstants.size_limit:
                 return AgentConstants.forward_left
@@ -184,15 +203,22 @@ class Chaser(object):
 
         return target_idx, target_size
 
-    def calculate_path_consistent_matrix(self, matrix):
-
+    def calculate_path_consistent_matrix(self, map_matrix):
+        '''
+        This method will return the map matrix skewed by the most recent path.
+        The further the pixel is from the most recent path, the higher the cost
+        of this pixel is. Therefore, paths that are similar to
+        the most recent path are preferable than very distinct paths to the agent.
+        :param map_matrix:
+        :return:
+        '''
         path_idx = np.array(self.newest_path)[:, ::-1]
         for ii in np.arange(AgentConstants.resolution):
             for jj in np.arange(AgentConstants.resolution):
-                if matrix[ii, jj] == 1:
-                    matrix[ii, jj] = 1 + abs(path_idx - [ii, jj]).sum(axis=1).min() / 2
+                if map_matrix[ii, jj] == 1:
+                    map_matrix[ii, jj] = 1 + abs(path_idx - [ii, jj]).sum(axis=1).min() / 2
 
-        return matrix
+        return map_matrix
 
     def reset(self):
         self.newest_target_idx = None
