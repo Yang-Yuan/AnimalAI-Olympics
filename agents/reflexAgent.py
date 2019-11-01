7e2dfe5c09ce8100ac4dfdfc0adf364739303281
ryan.y.yang <ryan.y.yang@outlook.com>
2019-10-31 21:13:58 -0500

    asdfasdf

---
+++ agents/reflexAgent.py
@@ -0,0 +1,131 @@
+import numpy as np
+from skimage import measure
+
+
+class Agent(object):
+
+    green = [0.506, 0.749, 0.255]
+    green_h = 1.4919028340080973
+
+    color_diff_limit = 0.45
+    position_diff_limit = 1
+    size_limit = 5
+
+    center_of_view = [41.5, 41.5]
+    aim_error_limit = 5
+    hl = 2
+
+    def __init__(self):
+        """
+         Load your agent here and initialize anything needed
+         WARNING: any path to files you wish to access on the docker should be ABSOLUTE PATHS
+        """
+        self.t = 0
+        self.step_n = 0
+        self.total_reward = 0
+        self.pirouette_step_n = 0
+
+    def reset(self, t=250):
+        """
+        Reset is called before each episode begins
+        Leave blank if nothing needs to happen there
+        :param t the number of timesteps in the episode
+        """
+        self.t = t
+        self.step_n = 0
+        self.total_reward = 0
+        self.pirouette_step_n = 0
+
+    def step(self, obs, reward, done, info):
+        """
+        A single step the agent should take based on the current state of the environment
+        We will run the Gym environment (AnimalAIEnv) and pass the arguments returned by env.step() to
+        the agent.
+
+        Note that should if you prefer using the BrainInfo object that is usually returned by the Unity
+        environment, it can be accessed from info['brain_info'].
+
+        :param obs: agent's observation of the current environment
+        :param reward: amount of reward returned after previous action
+        :param done: whether the episode has ended.
+        :param info: contains auxiliary diagnostic information, including BrainInfo.
+        :return: the action to take, a list or size 2
+        """
+        if done:
+            return [0, 0]
+
+        # Jaja, magic code!
+        if self.pirouette_step_n > 70:
+            self.pirouette_step_n = 0
+            return [1, 0]
+
+        obs_visual, obs_vector = obs
+        diff_green = abs(Agent.toHueImage(obs_visual) - Agent.green_h)
+        is_green = diff_green < Agent.color_diff_limit
+
+        print(obs_vector)
+
+        if not is_green.any():
+            self.pirouette_step_n += 1
+            return [0, 1]
+
+        if 1 == is_green.sum():
+            diff_center = np.array(np.where(is_green)).transpose()[0] - Agent.center_of_view
+            target_size = 1
+        else:
+            labels, label_num = measure.label(input = is_green, background = False, return_num = True, connectivity = 1)
+            sizes = [(labels == label).sum() for label in range(1, label_num + 1)]
+            target_label = np.argmax(sizes) + 1
+            center_of_target = np.array(np.where(labels == target_label)).mean(axis = 1)
+            diff_center = center_of_target - Agent.center_of_view
+            target_size = sizes[target_label - 1]
+
+        if diff_center[1] < -Agent.aim_error_limit * (1 + np.exp(-target_size / Agent.hl)):
+            if target_size < Agent.size_limit:
+                self.pirouette_step_n = 0
+                return [1, 2]
+            else:
+                self.pirouette_step_n += 1
+                return [0, 2]
+        elif diff_center[1] > Agent.aim_error_limit * (1 + np.exp(-target_size / Agent.hl)):
+            if target_size < Agent.size_limit:
+                self.pirouette_step_n = 0
+                return [1, 1]
+            else:
+                self.pirouette_step_n += 1
+                return [0, 1]
+        else:
+            self.pirouette_step_n = 0
+            return [1, 0]
+
+    @staticmethod
+    def toHueImage(img):
+        h_img = np.zeros(img.shape[0 : 2])
+        for ii in range(img.shape[0]):
+            for jj in range(img.shape[1]):
+                h_img[ii, jj] = Agent.toHue(img[ii, jj])
+
+        return h_img
+
+
+    @staticmethod
+    def toHue(rgb):
+        ind_min = rgb.argmin()
+        ind_max = rgb.argmax()
+        diff = rgb[ind_max] - rgb[ind_min]
+
+        if 0 == diff:
+            return 0
+
+        if 0 == ind_max:
+            h = (rgb[1] - rgb[2]) / diff
+        elif 1 == ind_max:
+            h = 2 + (rgb[2] - rgb[0]) / diff
+        else:
+            h = 4 + (rgb[0] - rgb[1]) / diff
+
+        if h < 0:
+            h += 6
+
+        return h
+
